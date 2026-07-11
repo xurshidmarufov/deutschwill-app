5f73665e32f53ec97d118b0a66a59e21cc71c369
xurshidmarufov <xurwidmarufov@gmail.com>
2026-07-11 23:51:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // Deutsch Will Scripts
+
+const startBtn = document.getElementById("startChat");
+
+if (startBtn) {
+    startBtn.addEventListener("click", () => {
+        window.location.href = "pages/conversation.html";
+    });
+}
