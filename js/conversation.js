c391a04aee5c984f35ce972d3045e94fe9a87818
xurshidmarufov <xurwidmarufov@gmail.com>
2026-07-12 17:45:19 +0500

    Update conversation.js

--- js/conversation.js
+++ js/conversation.js
@@ -1,9 +1,13 @@
 const particlesContainer = document.querySelector(".orb-particles");
+
 for(let i = 0; i < 20; i++){
 
     const particle = document.createElement("span");
 
     particle.classList.add("particle");
+
+    particle.style.left = Math.random() * 100 + "%";
+    particle.style.top = Math.random() * 100 + "%";
 
     particlesContainer.appendChild(particle);
 
