8f13ed420fb5685b67b6fca2d1cf9e99c7922bac
xurshidmarufov <xurwidmarufov@gmail.com>
2026-07-13 22:16:31 +0500

    Update conversation.js

--- js/conversation.js
+++ js/conversation.js
@@ -1,7 +1,37 @@
-console.log("NEW conversation.js");
-
 const canvas = document.getElementById("orbCanvas");
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d", { alpha: true });
 
-ctx.fillStyle = "red";
-ctx.fillRect(0, 0, 200, 200);
+const SIZE = 200;
+const DPR = window.devicePixelRatio || 1;
+
+function resizeCanvas() {
+
+    canvas.width = SIZE * DPR;
+    canvas.height = SIZE * DPR;
+
+    canvas.style.width = SIZE + "px";
+    canvas.style.height = SIZE + "px";
+
+    ctx.setTransform(DPR, 0, 0, DPR, 0, 0);
+
+}
+
+resizeCanvas();
+
+const CONFIG = {
+
+    size: SIZE,
+    center: SIZE / 2,
+    fps: 60,
+
+    blobCount: 8,
+
+    minRadius: 88,
+    maxRadius: 110,
+
+    orbitRadius: 24
+
+};
+
+let time = 0;
+
