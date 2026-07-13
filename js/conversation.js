a1a4ae074f66a6e767c95fa0b695098e62505795
xurshidmarufov <xurwidmarufov@gmail.com>
2026-07-13 21:59:37 +0500

    Create conversation.js

---
+++ js/conversation.js
@@ -0,0 +1,7 @@
+console.log("NEW conversation.js");
+
+const canvas = document.getElementById("orbCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "red";
+ctx.fillRect(0, 0, 200, 200);
